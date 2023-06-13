bb10dce0f5e3cb58043ae176a6fd3e2c3175abea
Romain DELCHIE <rdelchie@artem.local>
2023-06-13 18:16:07 +0200

    feat(data): add data seeding

---
+++ data/seeding.sql
@@ -0,0 +1,38 @@
+BEGIN;
+TRUNCATE TABLE "quotation_has_product", "product", "range", "quotation", "delivery", "user", "profile" RESTART IDENTITY;
+
+INSERT INTO "profile" ("name", "discount")
+VALUES
+("utilisateur", "2.5"),
+("revendeur", "1");
+
+
+INSERT INTO "user" ("email", "company", "firstname", "lastname", "phone_number", "password", "invoice_address", "role", "profile_id")
+VALUES
+  ('test@test.fr', "Techni-fournil" 'Jean', 'Garnier', '0123456789', '$2b$10$NW8QoaLybPfVjJ5Q5zDJ/ucqL8WZEiMQZoVvl8OSXU5OHF3CxWEqK', '1 Boulevard de Belleville 75000 Paris', 'user', '2'),
+  ('arnaud.toury@artem-fr.com', "Artem" 'Arnaud', 'Toury', '0164112461', '$2b$10$NW8QoaLybPfVjJ5Q5zDJ/ucqL8WZEiMQZoVvl8OSXU5OHF3CxWEqK', '16 rue de Berlin 77144 Montévrain', 'admin', '2'),
+  ('test1@test.fr', "Boulangerie de la gare" 'Frederic', 'Babin', '0160142584', '$2b$10$NW8QoaLybPfVjJ5Q5zDJ/ucqL8WZEiMQZoVvl8OSXU5OHF3CxWEqK', '35 rue de la gare 85000 La roche sur yon', '1');
+  
+
+INSERT INTO "delivery" ("delivery_address", "user_id")
+VALUES
+  ('Boulangerie Mon client, 10 rue du client, 75012 Paris', 1);
+
+INSERT INTO "quotation" ("user_id")
+VALUES
+  (1);
+
+INSERT INTO "range" ("name", "description", "image_link")
+VALUES
+  ('Toile enfourneur', `Découvrez nos toiles enfourneur en coton de qualité supérieure, résistantes et durables. Avec des ourlets rentrés pour éviter l'effilochage, ces toiles sont confectionnées avec un fil Kevlar résistant aux hautes températures. Fermeture de toile à recouvrement, cordon ou sangle avec barre inox sertie ou non suivant vos préférences. Profitez de leur fonctionnalité optimale. Choisissez la qualité et notre expertise pour des performances inégalées.`),
+  ('Toile de couche', `Découvrez notre gamme de toiles de couche pour la boulangerie. Choisissez parmi nos options de haute qualité, notamment la toile Arlin en 100% lin (380g/m2) matière utilisée communément pour les toiles de couche, et la toile Arconet, un mélange de lin, coton et polyester (320g/m2) avec traitement hydrofuge et anti bactérien pour limiter le farinage et favoriser un séchage rapide. Disponibles en rouleaux de 50m ou en toiles confectionnées avec ourlets ou ourlets et cordons. La solution idéale pour une utilisation professionnelle en boulangerie.`);
+
+INSERT INTO "product" ("reference", "name", "designation", "description", "image_link", "brand", "price", "unit", "weight", "delivery_time", "stock", "range_id")
+VALUES
+  ('TE_70X30_3S_B', 'Toile enfourneur', 'Toile Enfour. BONGARD CS8T (3050 x 705) 3 sangles + boucles + barres', 'Toile enfourneur en Coton 500g/m2 à 3 sangles avec barres inox serties pour four Bongard réf: CS8T', 'TE_Bong.jpg', 'Bongard', 58.07, 1, 2.5,'0 jours', 'true', 1);
+  
+INSERT INTO "quotation_has_product" ("quotation_id", "product_id", "quantity")
+VALUES
+  (1, 1, 2);
+  
+COMMIT;
--- data/tables_creation.sql
+++ data/tables_creation.sql
@@ -15,7 +15,7 @@ CREATE TABLE "user" (
   "phone_number" TEXT,
   "password" TEXT NOT NULL,
   "invoice_address" TEXT NOT NULL,
-  "role" TEXT NOT NULL DEFAULT 'user' CHECK ("role" IN ('user', 'admin'))
+  "role" TEXT NOT NULL DEFAULT 'user' CHECK ("role" IN ('user', 'admin')),
   "profile_id" INT NOT NULL REFERENCES "profile"("id") ON DELETE CASCADE
 );
 
@@ -28,7 +28,7 @@ CREATE TABLE "delivery" (
 CREATE TABLE "quotation" (
   "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "creation_date" DATE NOT NULL DEFAULT CURRENT_DATE,
-  "expiration_date" DATE NOT NULL DEFAULT DATEADD(MONTH, 2, "creation_date")
+  "expiration_date" DATE,
   "user_id" INT NOT NULL REFERENCES "user"("id") ON DELETE CASCADE
 );
 
@@ -36,7 +36,7 @@ CREATE TABLE "range" (
   "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "name" TEXT NOT NULL,
   "description" TEXT NOT NULL,
-  "image_link" TEXT NOT NULL,
+  "image_link" TEXT NOT NULL
 );
 
 CREATE TABLE "product" (
@@ -49,19 +49,33 @@ CREATE TABLE "product" (
   "brand" TEXT NOT NULL,
   "price" NUMERIC(10, 2) NOT NULL,
   "unit" NUMERIC(10, 2) NOT NULL,
+  "weight" NUMERIC(10, 2) NOT NULL,
   "delivery_time" TEXT NOT NULL,
   "stock" BOOLEAN NOT NULL,
   "range_id" INT NOT NULL REFERENCES "range"("id") ON DELETE CASCADE
-
-
 );
 
 CREATE TABLE "quotation_has_product" (
   "quotation_id" INT REFERENCES "quotation"("id") ON DELETE CASCADE,
   "product_id" INT REFERENCES "product"("id") ON DELETE CASCADE,
   PRIMARY KEY("quotation_id", "product_id"),
-  "quantity" NUMERIC(10, 2) NOT NULL,
+  "quantity" NUMERIC(10, 2) NOT NULL
 );
+
+-- Création de la fonction déclenchée pour calculer la valeur par défaut de "expiration_date"
+CREATE OR REPLACE FUNCTION set_default_expiration_date()
+  RETURNS TRIGGER AS $$
+BEGIN
+  NEW.expiration_date := NEW.creation_date + INTERVAL '2 months';
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Création du déclencheur (trigger) pour appeler la fonction déclenchée lors de l'insertion
+CREATE TRIGGER set_expiration_date_trigger
+  BEFORE INSERT ON "quotation"
+  FOR EACH ROW
+  EXECUTE FUNCTION set_default_expiration_date();
 
 
 COMMIT;
