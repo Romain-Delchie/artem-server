360b17171c06e47c5d76a0f2772c1ce77c860a88
Romain DELCHIE <rdelchie@artem.local>
2023-08-01 10:16:17 +0200

    fix(product.controller): fix return getproducts

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -6,9 +6,8 @@ const productController = {
 
   async getProducts(req, res) {
     const products = await product.findAll();
-    console.log(products)
-    return res.json({ products });
-    
+    return res.json(products);
+
   },
 
   async addProduct(req, res) {
@@ -16,14 +15,14 @@ const productController = {
     return res.status(201).json({ newProduct });
   },
 
-  
+
   async getOneProduct(req, res) {
     const oneProduct = await product.findByPk(req.params.id);
     if (!oneProduct) {
-        throw new ArtemError('Product not found', 404);
+      throw new ArtemError('Product not found', 404);
     }
     return res.json({ oneProduct });
-    },
+  },
 
   async patchProduct(req, res) {
     const productToPatch = await product.findByPk(req.params.id);
@@ -31,7 +30,7 @@ const productController = {
     if (!productToPatch) {
       res.status(404).json({ error: "product not found" });
     }
-    
+
     const patchedproduct = await product.update({ id: req.params.id, ...req.body });
     return res.json({ product: patchedproduct });
   },
@@ -39,7 +38,7 @@ const productController = {
   async deleteProduct(req, res) {
     const productToDelete = await product.findByPk(req.params.id);
     if (!productToDelete) {
-        throw new ArtemError('Product not found', 404);
+      throw new ArtemError('Product not found', 404);
     }
     await product.delete(req.params.id);
     return res.status(204).json();
